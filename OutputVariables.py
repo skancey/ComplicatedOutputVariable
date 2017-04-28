3e2ef21060fe650c99979725db0d458954a02b65
skancey <siddiqkancey@gmail.com>
2017-04-28 12:31:53 -0400

    Add files via upload

---
+++ OutputVariables.py
@@ -0,0 +1,9 @@
+# file: OutputVariables.py
+
+first_name = 'Bob'
+last_name = 'Newhard'
+pay_rate = 25.25
+hours_worked = 45
+bob_pay = pay_rate * hours_worked
+
+print first_name, last_name, "has worked", hours_worked, "hours at $", pay_rate, "per hour for a total of $", bob_pay
